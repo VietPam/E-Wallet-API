c8efea094f4e46d3a45d8a4677a7f32f04fd6442
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-10-27 22:39:14 +0700

    init project

---
+++ src/test/java/com/quangviet/EWallet/API/EWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangviet.EWallet.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
