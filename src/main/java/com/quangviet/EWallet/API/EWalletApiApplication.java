e33089ccf2c4ee1736dd9eb229d72c5177ca39cd
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-10-27 23:14:21 +0700

    added mysql specific properties

--- src/main/java/com/quangviet/EWallet/API/EWalletApiApplication.java
+++ src/main/java/com/quangviet/EWallet/API/EWalletApiApplication.java
@@ -2,8 +2,9 @@ package com.quangviet.EWallet.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
+//@EnableSwagger2
 public class EWalletApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/quangviet/EWallet/API/entity/User.java
@@ -0,0 +1,8 @@
+package com.quangviet.EWallet.API.entity;
+
+import jakarta.persistence.Entity;
+
+//@Entity
+public class User {
+    
+}
